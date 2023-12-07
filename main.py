a03ba44c63a1815b0b462bdcd8104799b878355c
JoshuaGuevaraJ1 <joguevara84@gmail.com>
2023-12-06 23:57:42 -0600

    Característica: Ahora encuentra numeros pares e impares

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Version 1.0 29-Agosto-2023
+#Version 2.0 31-Agosto-2023
 
 # Declaracion de variables e inicializacion
 import os
@@ -6,9 +6,11 @@ suma = 0
 mayor = 0
 menor = 32767   #Se coloca este rango porque es el tamaño máximo de un tipo dato entero
 contador = 0
+pares = 0
+inpares = 0
 
 while True:
-    # Saber si el numero es valido
+    # Saber si el numero es valido (1)
     while True:
         numero = int(input("Ingresa un número: "))
         if (numero<0 or numero>32767):
@@ -25,6 +27,12 @@ while True:
         mayor = numero
     if numero < menor:
         menor = numero
+
+    # Cuenta numeros inpares y pares ingresados en total mediante el módulo por valor ingresado
+    if numero%2 == 1:
+        pares += 1
+    if numero%2 == 0:
+        inpares+=1
     
     opc = input("¿Más datos?\n (S/N)\t")
     if opc=="N" or opc=="n":
@@ -37,3 +45,5 @@ print("Se ingresaron {} números".format(contador))
 print("El promedio es ",promedio)
 print("El número mayor ingresado es ",mayor)
 print("El número menor ingresado es ",menor)
+print("En total se ingresaron {} números inpares".format(inpares))
+print("En total se ingresaron {} números pares".format(pares))
